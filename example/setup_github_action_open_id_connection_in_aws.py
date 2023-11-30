301fb30f0649fff6d33b3b1c0fb071fb0fb62ecf
Sanhe <MacHu-GWU@users.noreply.github.com>
2023-11-30 04:16:09 -0500

    add test to switch aws credential

--- example/setup_github_action_open_id_connection_in_aws.py
+++ example/setup_github_action_open_id_connection_in_aws.py
@@ -7,10 +7,15 @@ It will create the OIDC provider in AWS IAM.
 
 from gh_action_open_id_in_aws.impl import setup_github_action_open_id_connection_in_aws
 
-setup_github_action_open_id_connection_in_aws(
-    aws_profile="bmt_app_dev_us_east_1",
-    stack_name="gh-action-open-id-in-aws",
-    github_org="MacHu-GWU",
-    github_repo="gh_action_open_id_in_aws-project",
-    role_name="gh_action_open_id_in_aws",
-)
+for aws_profile in [
+    "bmt_app_dev_us_east_1",
+    "bmt_app_test_us_east_1",
+    "bmt_app_prod_us_east_1",
+]:
+    setup_github_action_open_id_connection_in_aws(
+        aws_profile=aws_profile,
+        stack_name="gh-action-open-id-in-aws",
+        github_org="MacHu-GWU",
+        github_repo="gh_action_open_id_in_aws-project",
+        role_name="gh_action_open_id_in_aws",
+    )
