e1b3b50bbfe45e41b8f24df8b41753cc6930a3ae
Sanhe <MacHu-GWU@users.noreply.github.com>
2023-11-30 02:35:09 -0500

    Create setup_cross_aws_account_iam_role_project.py

---
+++ example/setup_cross_aws_account_iam_role_project.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+"""
+Setup GitHub Action AWS permission forhttps://github.com/MacHu-GWU/cross_aws_account_iam_role-project.
+It will REUSE the existing OIDC provider in AWS IAM.
+"""
+
+from boto_session_manager import BotoSesManager
+from gh_action_open_id_in_aws.impl import setup_github_action_open_id_connection_in_aws
+
+aws_profile = "bmt_app_dev_us_east_1"
+bsm = BotoSesManager(profile_name=aws_profile)
+role_name = "cross-aws-account-iam-role-github-open-id-connection"
+
+setup_github_action_open_id_connection_in_aws(
+    aws_profile=aws_profile,
+    stack_name="cross-aws-account-iam-role-github-open-id-connection",
+    github_org="MacHu-GWU",
+    github_repo="cross_aws_account_iam_role-project",
+    role_name=role_name,
+    oidc_provider_arn=f"arn:aws:iam::{bsm.aws_account_id}:oidc-provider/token.actions.githubusercontent.com",
+)
