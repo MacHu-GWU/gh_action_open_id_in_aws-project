882a462267a6afe54cdfe317cca30819bebeeae6
Sanhe <MacHu-GWU@users.noreply.github.com>
2023-11-30 14:02:23 -0500

    chore

--- test_github_action_open_id_in_aws.py
+++ test_github_action_open_id_in_aws.py
@@ -7,6 +7,7 @@ AWS Account id.
 
 import boto3
 
+# print the current AWS principal ARN (AWS account id masked)
 res = boto3.client("sts").get_caller_identity()
 parts = res["Arn"].split(":")
 parts[4] = parts[4][:2] + "********" + parts[4][-2:]
